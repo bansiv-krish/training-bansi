fef56ad3026339c91f32de556af761352177ead7
bansiv-krish <bansi.vithlani@krishtechnolabs.com>
2020-12-28 19:53:40 +0530

    Customer layout three column

---
+++ app/code/TrainingBansi/CustomLayout/Controller/Index/Index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace TrainingBansi\CustomLayout\Controller\Index;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Catalog index page controller.
+ */
+class Index extends \Magento\Framework\App\Action\Action
+{
+    
+    protected $pageFactory;
+    
+    public function __construct(PageFactory $pageFactory, Context $context)
+    {
+        $this->pageFactory=$pageFactory;
+        parent::__construct($context);
+    }
+    
+    public function execute()
+    {
+        
+        return $this->pageFactory->create();
+    }
+}
