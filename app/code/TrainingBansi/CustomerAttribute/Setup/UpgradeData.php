4efe841f6d4aadaad6629e1a6c1447ed7e806636
bansiv-krish <bansi.vithlani@krishtechnolabs.com>
2020-12-29 19:40:59 +0530

    Customer Attribute in frontend and Backend

--- app/code/TrainingBansi/CustomerAttribute/Setup/UpgradeData.php
+++ app/code/TrainingBansi/CustomerAttribute/Setup/UpgradeData.php
@@ -1,5 +1,5 @@
 <?php
-namespace Ucs\CustomerAttribute\Setup;
+namespace TrainingBansi\CustomerAttribute\Setup;
 
 use Magento\Customer\Model\Customer;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
@@ -7,7 +7,8 @@ use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Customer\Setup\CustomerSetupFactory;
 
-class UpgradeData implements UpgradeDataInterface{
+class UpgradeData implements UpgradeDataInterface
+{
 
     private $customerSetupFactory;
 
@@ -20,18 +21,16 @@ class UpgradeData implements UpgradeDataInterface{
 /**
  * {@inheritdoc}
  */
-public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context){
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if (version_compare($context->getVersion(), '0.0.5', '<')) {
+              $setup->startSetup();
 
-    $setup->startSetup();
-
-    $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
-
-
-
-        $customerSetup->addAttribute(
-                    \Magento\Customer\Model\Customer::ENTITY,
-                    "father_name",                    
-                    [
+              $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+              $customerSetup->addAttribute(
+                  \Magento\Customer\Model\Customer::ENTITY,
+                  "father_name",
+                  [
                         'type' => 'text',
                         'label' => "Father's Name",
                         'input' => 'text',
@@ -41,19 +40,23 @@ public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface 
                         'sort_order' => 1000,
                         'position' => 1000,
                         'system' => 0,
+                        'is_used_in_grid'=> true,
+                        'is_visible_in_grid'=> true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => true,
                     ]
-                );
+              );
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'father_name')
-        ->addData(['used_in_forms' => [
-            'adminhtml_customer',
-            'adminhtml_checkout',
-            'customer_account_create',
-            'customer_account_edit'
-        ]]);
-        $attribute->save();
+              $attribute = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'father_name')
+              ->addData(['used_in_forms' => [
+               'adminhtml_customer',
+               'adminhtml_checkout',
+               'customer_account_create',
+               'customer_account_edit'
+              ]]);
+              $attribute->save();
 
-        $customerSetup->addAttribute(Customer::ENTITY, 'mother_name', [
+              $customerSetup->addAttribute(Customer::ENTITY, 'mother_name', [
                     'type' => 'varchar',
                     'label' => "Mother’s Name",
                     'input' => 'text',
@@ -63,18 +66,21 @@ public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface 
                     'sort_order' => 1000,
                     'position' => 1000,
                     'system' => 0,
+                    'is_used_in_grid'=> true,
+                    'is_visible_in_grid'=> true,
+                    'is_filterable_in_grid' => true,
+                    'is_searchable_in_grid' => true,
                 ]);
 
-        $attribute = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'mother_name')
-        ->addData(['used_in_forms' => [
-            'adminhtml_customer',
-            'adminhtml_checkout',
-            'customer_account_create',
-            'customer_account_edit'
-        ]]);
-        $attribute->save();
+              $attribute = $customerSetup->getEavConfig()->getAttribute(\Magento\Customer\Model\Customer::ENTITY, 'mother_name')
+              ->addData(['used_in_forms' => [
+               'adminhtml_customer',
+               'adminhtml_checkout',
+               'customer_account_create',
+               'customer_account_edit'
+              ]]);
+              $attribute->save();
 
-
-    
-}
+        }
+    }
 }
